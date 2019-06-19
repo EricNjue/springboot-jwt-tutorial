111c5c02843baba3d5c74c5a5056fd5c82626f07
Eric Gitonga Njue <eric.gitonga38@gmail.com>
2019-06-19 12:51:08 +0300

    Uses Hardcoded user and password values. i.e Username: javainuse Password: password

---
+++ src/main/java/com/devericnjue/springbootjwttutorial/SpringbootJwtTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.devericnjue.springbootjwttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtTutorialApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootJwtTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devericnjue/springbootjwttutorial/SpringbootJwtTutorialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devericnjue.springbootjwttutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJwtTutorialApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
